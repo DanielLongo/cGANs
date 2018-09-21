2294d150a6af56b8d5674cf5a6b1fa87f30edaff
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-09-20 19:50:14 -0700

    EMNIST

--- trans_DCGAN.py
+++ trans_DCGAN.py
@@ -28,9 +28,9 @@ transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])
 
-mnist_train = torchvision.datasets.MNIST('./MNIST_data', train=True, download=True, transform=transform)
+mnist_train = torchvision.datasets.EMNIST('./EMNIST_data', train=True, download=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True)
-mnist_test = torchvision.datasets.MNIST('./MNIST_data', train=False, download=True, transform=transform)
+mnist_test = torchvision.datasets.EMNIST('./EMNIST_data', train=False, download=True, transform=transform)
 test_loader = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size,  shuffle=True)
 
 pretrained_generator = ConditionalGenerator()
